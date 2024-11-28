90a8009a94f1f0a82cccbf311119138ae9eb84a6
Edithhh <124547125+edithh81@users.noreply.github.com>
2024-11-28 19:18:06 +0700

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,35 +1,7 @@
 from parameter import *
 
-class BitField:
-    def __init__(self, num_pieces):
-        self.bitfield = bytearray([0] * ((num_pieces + 7) // 8))
-        self.num_pieces = num_pieces
-
-    def set_piece(self, piece_index):
-        if 0 <= piece_index < self.num_pieces:
-            byte_index = piece_index // 8
-            bit_offset = 7 - (piece_index % 8)
-            self.bitfield[byte_index] |= (1 << bit_offset)
-
-    def has_piece(self, piece_index):
-        if 0 <= piece_index < self.num_pieces:
-            byte_index = piece_index // 8
-            bit_offset = 7 - (piece_index % 8)
-            return bool(self.bitfield[byte_index] & (1 << bit_offset))
-
-    def count_pieces(self):
-        return sum(bin(byte).count('1') for byte in self.bitfield)
-
-    def to_bytes(self):
-        return bytes(self.bitfield)
-
-    @staticmethod
-    def from_bytes(data, num_pieces):
-        bf = BitField(num_pieces)
-        bf.bitfield = bytearray(data)
-        return bf
-
 def recvall(sock, n):
+    # Helper function to recv n bytes or return None if EOF is hit
     data = bytearray()
     while len(data) < n:
         packet = sock.recv(n - len(data))
@@ -38,42 +10,24 @@ def recvall(sock, n):
         data.extend(packet)
     return data
 
-def send_msg(sock, msg):
-    try:
-        data = pickle.dumps(msg)
-        length = struct.pack('>I', len(data))
-        sock.sendall(length + data)
-    except Exception as e:
-        logging.error(f"Error sending message: {e}")
-        raise
-
 def recv_msg(sock):
-    try:
-        length_data = recvall(sock, 4)
-        if not length_data:
-            return None
-        length = struct.unpack('>I', length_data)[0]
-        return pickle.loads(recvall(sock, length))
-    except Exception as e:
-        logging.error(f"Error receiving message: {e}")
-        raise
+    # Receive the size of the message first
+    raw_msglen = recvall(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    # Now receive the message data
+    return recvall(sock, msglen)
+
+def send_msg(sock, msg):
+    msg = pickle.dumps(msg)
+    sock.sendall(struct.pack('>I', len(msg)) + msg)
 
 def sha1_hash(data):
     return hashlib.sha1(data).hexdigest()
 
-def calculate_num_pieces(file_size):
-    return (file_size + PIECE_SIZE - 1) // PIECE_SIZE
-
-def calculate_piece_size(file_size, piece_index, num_pieces):
-    if piece_index < num_pieces - 1:
-        return PIECE_SIZE
-    return file_size - (num_pieces - 1) * PIECE_SIZE
-
-def setup_logger(name):
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    handler = logging.FileHandler(f"{name}.log")
-    handler.setFormatter(logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-    logger.addHandler(handler)
-    return logger
+def generate_port():
+    # global PORT_PEER
+    # tmp = PORT_PEER
+    # PORT_PEER += 1
+    return random.randint(10000, 65535)
