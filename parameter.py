998dcf8ce649809073637d94cf98e9b00b4970d4
Edithhh <124547125+edithh81@users.noreply.github.com>
2024-11-28 19:17:51 +0700

    Update parameter.py

--- parameter.py
+++ parameter.py
@@ -8,57 +8,55 @@ import hashlib
 import struct
 import time
 import random
-from typing import Set, Dict, DefaultDict
-from collections import defaultdict
+from typing import Set, Dict
 import tkinter as tk
 from tkinter import messagebox, ttk
 from tkinter import filedialog
 import logging
 import json
 import shutil
+import queue
+from itertools import groupby
+import random
+PORT_PEER = 5500
 
-# Network Constants
-PIECE_SIZE = 512 * 1024  # 512KB per piece
-BLOCK_SIZE = 16384       # 16KB per block
-MAX_CONNECTIONS = 5
-MAX_PENDING_REQUESTS = 10
-KEEP_ALIVE_INTERVAL = 120  # 2 minutes
+PIECE_SIZE = 512 * 1024
+FORMAT = 'utf-8'
 
-# Message Types - Authentication
+HANDSHAKE = 'handshake'
+
 REGISTER = 'register'
 REGISTER_SUCCESS = 'register_success'
 REGISTER_FAIL = 'register_fail'
+
 LOGIN = 'login'
 LOGIN_SUCCESS = 'login_success'
 LOGIN_FAIL = 'login_fail'
-LOGOUT = 'logout'
+LOGIN_WRONG_PASSWORD = 'login_wrong_password'
+LOGIN_NOT_FOUND = 'login_not_found'
 
-# Message Types - File Operations
+LOGOUT = 'logout'
+LOGOUT_SUCCESS = 'logout_success'
+
 PUBLISH = 'publish'
 PUBLISH_SUCCESS = 'publish_success'
+PUBLISH_FAIL = 'publish_fail'
+
 FETCH = 'fetch'
 FETCH_SUCCESS = 'fetch_success'
+FETCH_FAIL = 'fetch_fail'
+
 GET_FILES = 'get_files'
 GET_FILES_SUCCESS = 'get_files_success'
+GET_FILES_FAIL = 'get_files_fail'
 
-# Message Types - Piece Transfer
-HANDSHAKE = 'handshake'
-BITFIELD = 'bitfield'
-REQUEST = 'request'
-PIECE = 'piece'
-HAVE = 'have'
-INTERESTED = 'interested'
-NOT_INTERESTED = 'not_interested'
-CHOKE = 'choke'
-UNCHOKE = 'unchoke'
-CANCEL = 'cancel'
+GET_PIECE = 'get_piece'
+GET_PIECE_SUCCESS = 'get_piece_success'
+GET_PIECE_FAIL = 'get_piece_fail'
 
-# Message Types - Piece Status
-UPDATE_BITFIELD = 'update_bitfield'
-PIECE_STATUS = 'piece_status'
+UPDATE_BITFIELD = 'UPDATE_BITFIELD'
+UPDATE_BITFIELD_SUCCESS = 'UPDATE_BITFIELD_SUCCESS'
+UPDATE_BITFIELD_FAIL = 'UPDATE_BITFIELD_FAIL'
 
-# Transfer States
-DOWNLOADING = 'downloading'
-UPLOADING = 'uploading'
-COMPLETED = 'completed'
-FAILED = 'failed'
+
+UPDATE_PIECE_POINT = 'UPDATE_PIECE_POINT'
